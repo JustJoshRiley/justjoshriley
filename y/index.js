939cbac97a2b31f9bb9cc4bd3060de69c9322bbc
justjoshriley <joshriley316@gmail.com>
2021-02-17 00:23:55 -0800

    new styling for landing

--- y/index.js
+++ y/index.js
@@ -1,18 +1,18 @@
-window.onload = function(){
-    document.getElementById('loading-screen').style.display = "none"; 
+window.onload = function() {
+    document.getElementById('loading-screen').style.display = "none";
 };
 
-function showProjects() {
-    var x = document.getElementById("projects");
-    var y = document.getElementById("header-info-box")
-    var z = document.getElementById("social-links-box")
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        y.style.display = "none";
-        z.style.display = "none";
-    } else {
-        x.style.display = "none";
-        y.style.display = "inherit";
-        z.style.display = "inherit";
-    }
-}
+// function showProjects() {
+//     var x = document.getElementById("projects");
+//     var y = document.getElementById("header-info-box")
+//     var z = document.getElementById("social-links-box")
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//         y.style.display = "none";
+//         z.style.display = "none";
+//     } else {
+//         x.style.display = "none";
+//         y.style.display = "inherit";
+//         z.style.display = "inherit";
+//     }
+// }
